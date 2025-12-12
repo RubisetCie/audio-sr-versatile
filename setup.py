8558a2ca7487d211ea558d3126e09caa87a129df
Matthieu Carteron <rubisetcie@gmail.com>
2025-12-12 12:59:55 +0100

    Added CI job to build wheel.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ REQUIRED = [
     "chardet",
     "numpy",
     "soundfile",
-    "librose",
+    "librosa",
     "scipy",
     "pandas",
     "unidecode",
@@ -72,7 +72,7 @@ except FileNotFoundError:
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    project_slug = NAME
     with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
@@ -140,7 +140,7 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "License :: OSI Approved :: MIT License",
+        "MIT",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: Implementation :: CPython",
